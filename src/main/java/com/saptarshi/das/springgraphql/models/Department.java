bb3a7ab5c61d82117cf5d871079976418e36ac00
Saptarshi Das <saptarshidas743@gmail.com>
2023-11-14 00:24:50 +0530

    Add relation between Employee and Department entities
    Add relation between Employee and Department entities for better accessibility.

--- src/main/java/com/saptarshi/das/springgraphql/models/Department.java
+++ src/main/java/com/saptarshi/das/springgraphql/models/Department.java
@@ -1,14 +1,20 @@
 package com.saptarshi.das.springgraphql.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -21,4 +27,9 @@ public class Department {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     private Long id;
     private String name;
+
+    @ToString.Exclude
+    @JsonManagedReference
+    @OneToMany(mappedBy = "department", fetch = FetchType.LAZY)
+    private List<Employee> employees;
 }
--- src/main/java/com/saptarshi/das/springgraphql/models/Employee.java
+++ src/main/java/com/saptarshi/das/springgraphql/models/Employee.java
@@ -1,14 +1,19 @@
 package com.saptarshi.das.springgraphql.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 
 @Data
@@ -25,4 +30,10 @@ public class Employee {
     private String email;
     private Long salary;
     private Long departmentId;
+
+    @ToString.Exclude
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "departmentId", insertable = false, updatable = false)
+    private Department department;
 }
