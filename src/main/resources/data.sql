407a52f2c7a063d2040bc65f04351fc7571a1a48
Saptarshi Das <saptarshidas743@gmail.com>
2023-11-14 00:22:28 +0530

    Add initial data for tables and update configuration
    Add initial data for tables and update application configuration to insert data after the tables are created and also log all the queries executed.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into department (id, name) values (1, 'Engineering');
+insert into department (id, name) values (2, 'Human Resource');
+insert into department (id, name) values (3, 'Finance');
+insert into department (id, name) values (4, 'Quality Control');
+insert into department (id, name) values (5, 'IT');
+
+insert into employee (id, name, email, salary, department_id) values (1, 'Amy', 'amy@mail.com', 20000, 1);
+insert into employee (id, name, email, salary, department_id) values (2, 'Britney', 'britney@mail.com', 30000, 2);
+insert into employee (id, name, email, salary, department_id) values (3, 'Charlie', 'charlie@mail.com', 40000, 3);
+insert into employee (id, name, email, salary, department_id) values (4, 'Daniel', 'daniel@mail.com', 50000, 4);
+insert into employee (id, name, email, salary, department_id) values (5, 'Evan', 'evan@mail.com', 60000, 5);
+insert into employee (id, name, email, salary, department_id) values (6, 'Freddy', 'freddy@mail.com', 70000, 1);
+insert into employee (id, name, email, salary, department_id) values (7, 'Gal', 'gal@mail.com', 80000, 1);
